1dbfd09e18ced8810df5e4ef9b3a9d5f594f7a1a
Hassan Hijjawi <hmh91@mail.aub.edu>
2022-10-29 09:37:39 +0300

    Added the specs and testcases

--- my_stack.cpp
+++ my_stack.cpp
@@ -1,5 +1,58 @@
-#include <vector>
+/*
+    int size ()
+
+    Requires: Nothing
+    Effects: Returns the number of elements in the stack.
+
+    Testing Strategy: 1) Stack has no elements.
+                      2) Stack has odd number of elements.
+                      3) Stack has even number of elements.
+
+    *****************************************************************  
+
+    bool isEmpty()
+    Requires: Nothing
+    Effects: Returns true is the stack has 0 elements, false otherwise.
+
+    Testing Strategy: 1) Empty stack.
+                      2) Stack contains n elements.
+
+    ********************************************************************************   
+    void push (const T &item)
+    Requires: Nothing.
+    Effects:  Saves a pointer to the passed value in the stack following the first-in first-out rule.
+
+    Testing Strategy:  1) Passing NULL.
+                       2) Executing the function an even number of times.
+                       3) Executing the function an odd number of times.
+
+    ********************************************************************************
+
+    T & top ()
+    Requires: Nothing.
+    Effects: Returns the value of the last added element in the stack.
+             If the stack was empty, it throws an error.
+
+    Testing Strategy: 1) Stack is initially empty.
+                      2) Stack contains an even number of elements.
+                      3) Stack contains an odd number of elements.
+                      4) Execute the function two or more consecutive runs.
+
+    ********************************************************************************
+    void pop()
+
+    Requires: Nothing.
+    Effects: Deletes the last added element to the stack only if it wasn't empty.
+
+    Testing Strategy: 1) Perform when stack is empty
+                      2) Perform when stack is not empty
+                      3) Keep on performing on a non-empty stack till it becomes empty.
+
+    *********************************************************************************                      
+*/
+
 #include <iostream>
+#include <vector>
 using namespace std;
 
 template <typename T>
